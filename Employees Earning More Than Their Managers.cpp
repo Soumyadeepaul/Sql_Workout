9983de2e648b36d630045c2d348747becf79b1ec
Soumyadeep Paul <92711266+Soumyadeepaul@users.noreply.github.com>
2025-10-06 07:06:42 +0530

    Update and rename Employees Earning More Than Their Managers.cpp to Employees Earning More Than Their Managers.sql

--- Employees Earning More Than Their Managers.cpp
+++
@@ -1,4 +0,0 @@
-//https://leetcode.com/problems/employees-earning-more-than-their-managers/
-
-# Write your MySQL query statement below
-select t1.name as Employee from employee as t1 join employee as t2 where t1.managerId=t2.id and t1.salary>t2.salary
