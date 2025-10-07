4207331cba42f43a96e97fed588b15aef56f82ff
Soumyadeep Paul <92711266+Soumyadeepaul@users.noreply.github.com>
2025-10-07 16:02:05 +0530

    Create Duplicate Emails.sql

---
+++ Duplicate Emails.sql
@@ -0,0 +1,4 @@
+--https://leetcode.com/problems/duplicate-emails/description/
+
+-- Write your MySQL query statement below
+select distinct p1.email from person as p1 join person as p2 where p1.email=p2.email and p1.id!=p2.id;
