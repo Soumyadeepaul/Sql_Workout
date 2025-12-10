5c4cd944a67f7b663356b5f694085d247f141530
Soumyadeep Paul <92711266+Soumyadeepaul@users.noreply.github.com>
2025-12-10 21:15:07 +0530

    Create Employees Whose Manager Left the Company.sql

---
+++ Employees Whose Manager Left the Company.sql
@@ -0,0 +1,6 @@
+--https://leetcode.com/problems/employees-whose-manager-left-the-company/?envType=study-plan-v2&envId=top-sql-50
+
+
+--# Write your MySQL query statement below
+
+select employee_id from employees where salary<30000 and manager_id not in (select employee_id from employees) order by employee_id
