5e51ca8ce2f91984ec0189816a34261391ec1aed
Soumyadeep Paul <92711266+Soumyadeepaul@users.noreply.github.com>
2025-12-18 21:01:09 +0530

    Create Game Play Analysis I.sql

---
+++ Game Play Analysis I.sql
@@ -0,0 +1,6 @@
+--https://leetcode.com/problems/game-play-analysis-i/
+
+
+--# Write your MySQL query statement below
+
+select player_id, min(event_date) first_login from activity group by player_id
