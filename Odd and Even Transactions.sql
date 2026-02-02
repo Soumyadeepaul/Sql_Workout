93593360deb8d89884049d06374ef58fdaa32ea1
Soumyadeep Paul <92711266+Soumyadeepaul@users.noreply.github.com>
2026-02-02 23:04:58 +0530

    Create Odd and Even Transactions.sql

---
+++ Odd and Even Transactions.sql
@@ -0,0 +1,7 @@
+--https://leetcode.com/problems/odd-and-even-transactions/description/
+
+
+--# Write your MySQL query statement below
+
+
+select transaction_date, sum(case when amount%2=0 then 0 else amount end) odd_sum, sum(case when amount%2=0 then amount else 0 end) even_sum from transactions group by transaction_date order by transaction_date asc
